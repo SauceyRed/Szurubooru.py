e970aefaa599faaa3e90aa065ff53d19cc5b8049
SauceyRed <42098474+SauceyRed@users.noreply.github.com>
2022-11-14 01:59:02 +0100

    Added more stuff to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,17 @@ setup(
 	version="0.1.0",
 	description="A Python API wrapper for Szurubooru",
 	author="SauceyRed",
-	license=""
+	author_email="csred@protonmail.com",
+	license="GPLv3",
+	classifiers=[
+		"Environment :: Console",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: 3.11"
+	]
 )
